a34930048e7b778d3068d459b58853c403badf35
jccastellanos <jccastellanos@grupoasd.com.co>
2021-08-05 22:03:45 -0500

    Inicializacion repo

---
+++ src/main/java/com/jcastellanos/test/GradleCiCdTestApplication.java
@@ -0,0 +1,13 @@
+package com.jcastellanos.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCiCdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCiCdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcastellanos/test/GradleCiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcastellanos.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleCiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
